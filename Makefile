d0793a9bd5e4581be70cbf99190399124b05b107
Admir Trakic <admir.trakic@persgroep.net>
2023-06-06 13:22:37 +0200

    Update

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
+CONTAINER := nginx-dynamic
 all:
 	shellcheck $$(git ls-files *.sh)
 	docker-compose up --build --force-recreate --remove-orphans -d
+	docker exec -it $(CONTAINER) nginx -t
+	docker exec -it $(CONTAINER) cat /etc/nginx/conf.d/default.conf
+	curl -fisk GET localhost:80
 
 clean:
 	docker-compose down -v
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+env
+
+_upstream_endpoint="${URL}"
+
 generate_conf () {
   cat >/etc/nginx/conf.d/default.conf <<-EOF
 server {
   listen 80;
-  listen  [::]:80;
-  
   server_name localhost;
   server_tokens off;
-
+  
   location / {
-    proxy_pass http://192.168.206.105:80; #### FIXME
+    proxy_pass $_upstream_endpoint;
+    #proxy_set_header Host \$host;
     proxy_set_header X-Real-IP \$remote_addr;
     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+  }
+
+  location /health-check {
+    add_header Content-Type text/plain;
+    return 200 "success";
   }
 }
 EOF
